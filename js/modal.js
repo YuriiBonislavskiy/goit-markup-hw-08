cc5e4e417695c04a1ebcc79fe855c11a68d360f0
YuriiBonislavskiy <130743571+YuriiBonislavskiy@users.noreply.github.com>
2023-05-26 22:18:53 +0300

    Домашнє завдання 8

--- js/modal.js
+++ js/modal.js
@@ -3,13 +3,16 @@
     openModalBtn: document.querySelector("[data-modal__open]"),
     closeModalBtn: document.querySelector("[data-modal__close]"),
     modal: document.querySelector("[data-modal]"),
+    menuBtnHidden: document.querySelector("[data-menu-button]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    refs.menuBtnHidden.classList.toggle("is-hidden");
     document.body.classList.toggle("modal-open");
     refs.modal.classList.toggle("is-hidden");
+
   }
 })();
